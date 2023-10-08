f932b87697dfc721ccf73d30a80030307e962506
sada335 <114182737+sada335@users.noreply.github.com>
2023-10-08 23:01:28 +0600

    Add files via upload

---
+++ Web Scrapping by Python/web_scrap_final_amazon.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+# Function to extract Product Title
+def get_title(soup):
+
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+        
+        # Inner NavigatableString Object
+        title_value = title.text
+
+        # Title as a string value
+        title_string = title_value.strip()
+
+    except AttributeError:
+        title_string = ""
+
+    return title_string
+
+
+# Function to extract Product Price
+def get_price(soup):
+
+    try:
+        price = soup.find("span", attrs={"class":'a-offscreen'}).string.strip()
+
+    except AttributeError:
+
+        try:
+            # If there is some deal price
+            price = soup.find("span", attrs={"class":'a-offscreen'}).string.strip()
+
+        except:
+            price = ""
+
+    return price
+
+# Function to extract Product Rating
+def get_rating(soup):
+
+    try:
+        rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+    
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+        except:
+            rating = ""	
+
+    return rating
+
+# Function to extract Number of User Reviews
+def get_review_count(soup):
+    try:
+        review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
+
+    except AttributeError:
+        review_count = ""	
+
+    return review_count
+
+
+# In[8]:
+
+
+if __name__ == '__main__':
+
+    # add your user agent 
+    HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+
+    # The webpage URL
+    URL = "https://www.amazon.com/s?k=playstation+4&i=todays-deals&ref=nb_sb_noss"
+
+    # HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
+
+    # Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "html.parser")
+
+    # Fetch links as List of Tag Objects
+    links = soup.find_all("a", attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
+
+    # Store the links
+    links_list = []
+
+    # Loop for extracting links from Tag Objects
+    for link in links:
+            links_list.append(link.get('href'))
+
+    d = {"title":[], "price":[], "rating":[], "reviews":[]}
+    
+    # Loop for extracting product details from each link 
+    for link in links_list:
+        new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
+
+        new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+
+        # Function calls to display all necessary product information
+        d['title'].append(get_title(new_soup))
+        d['price'].append(get_price(new_soup))
+        d['rating'].append(get_rating(new_soup))
+        d['reviews'].append(get_review_count(new_soup))
+
+    
+    amazon_df = pd.DataFrame.from_dict(d)
+    amazon_df['title'].replace('', np.nan, inplace=True)
+    amazon_df = amazon_df.dropna(subset=['title'])
+    amazon_df.to_csv("amazon_data.csv", header=True, index=False)
+
+
+# In[9]:
+
+
+amazon_df
+
+
+# In[ ]:
+
+
+
+
